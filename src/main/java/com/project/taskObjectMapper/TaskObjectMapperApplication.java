09b5e8d997b51b25f290a4c7e6de4bfd86c6d6fe
sterdexx1 <sterdexx1@gmail.com>
2026-01-13 16:44:15 +0300

    Initial commit

---
+++ src/main/java/com/project/taskObjectMapper/TaskObjectMapperApplication.java
@@ -0,0 +1,13 @@
+package com.project.taskObjectMapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskObjectMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskObjectMapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/taskObjectMapper/TaskObjectMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.taskObjectMapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskObjectMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
